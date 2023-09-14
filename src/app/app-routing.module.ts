e9465c236c3b298fc7c564f678984f36f8565806
abinz-25 <125503486+abinz-25@users.noreply.github.com>
2023-09-14 16:31:30 +0500

    updated project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { ResourceComponent } from './components/resource/resource.component';
+import { LinksComponent } from './components/links/links.component';
+import { BookComponent } from './components/book/book.component';
+import { MainComponent } from './pages/main/main.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('./pages/pages.module').then((m) => m.PagesModule),
-  },
+  { path: 'main', component:MainComponent},
+  { path: 'resource', component: ResourceComponent},
+  { path: 'links', component:LinksComponent },
+  { path: 'book', component:BookComponent}
 ];
 
 @NgModule({
--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import { ResourceComponent } from './resource/resource.component';
+import { LinksComponent } from './links/links.component';
+import { BookComponent } from './book/book.component';
+import { RouterModule } from '@angular/router';
+
 
 
 
 @NgModule({
   declarations: [
-    SideBarComponent
+    SideBarComponent,
+    ResourceComponent,
+    LinksComponent,
+    BookComponent,
+   
   ],
   imports: [
-    CommonModule
+    CommonModule,RouterModule
   ],
   exports: [
     SideBarComponent
